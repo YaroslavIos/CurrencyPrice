38e7206ed3edf9e488c90e00267f75b0e035b5fb
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-09-08 19:43:16 +0300

    Initial Commit

---
+++ CurrencyPrice/CurrencyPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyPriceApp.swift
+//  CurrencyPrice
+//
+//  Created by Ярослав Любиченко on 8.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
